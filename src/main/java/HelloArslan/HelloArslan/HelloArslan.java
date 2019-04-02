a86f7ae27127c4fa3ef0bb5d92ed301cf30f456a
Арслан <47584980+15170027@users.noreply.github.com>
2019-04-02 18:33:48 +0500

    Create HelloArslan.java

---
+++ src/main/java/HelloArslan/HelloArslan/HelloArslan.java
@@ -0,0 +1,13 @@
+package HelloArslan.HelloArslan;
+
+/**
+ * Hello world!
+ *
+ */
+public class HelloArslan 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
